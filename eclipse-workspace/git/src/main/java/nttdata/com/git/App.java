e5f3d56fd3395a23ea24f7dce95f49054ebd22b5
inmamesa <inmamesa98@gmail.com>
2022-02-07 10:10:50 +0100

    primer commit

---
+++ eclipse-workspace/git/src/main/java/nttdata/com/git/App.java
@@ -0,0 +1,13 @@
+package nttdata.com.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
