8af4d60d86c0854a813829b7f1d99fc6eadd6276
inmamesa <inmamesa98@gmail.com>
2022-02-09 11:14:34 +0100

    segundo commit

---
+++ Documents/SpringBoot/configServer/src/test/java/com/app/tfg/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
